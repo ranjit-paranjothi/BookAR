1841a78d884549013f1ce1c7dbc74985db104f1b
Ranjit <your-email@example.com>
2026-06-06 17:04:22 +0530

    file changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const fs = require('fs');
 
+const keyPath = path.join(__dirname, 'key.pem');
+const certPath = path.join(__dirname, 'cert.pem');
+const httpsConfig = fs.existsSync(keyPath) && fs.existsSync(certPath)
+  ? {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    }
+  : true;
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -17,10 +26,7 @@ module.exports = {
     compress: true,
     port: 8080,
     hot: true,
-    https: {
-      key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
-    },
+    https: httpsConfig,
   },
   plugins: [
     new HtmlWebpackPlugin({
